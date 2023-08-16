affa43a090feaab99873e76804b1f2aac71c0e47
ppopow <pawel.popow@gmail.com>
2023-08-16 21:16:17 +0100

    first commit

---
+++ age.py
@@ -0,0 +1,9 @@
+# deklarujemy i inicjalizujemy zmienne
+actYear = 2023
+pythonYear = 1989
+
+# obliczamy wiek Pythona
+agePython = actYear - pythonYear
+
+if __name__ == '__main__':
+    print(agePython)
