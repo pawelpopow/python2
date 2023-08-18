a440e79529aca9fed50cdcb86f48784b74257ff8
ppopow <pawel.popow@gmail.com>
2023-08-18 14:43:12 +0100

    two commit

--- age.py
+++ age.py
@@ -5,5 +5,11 @@ pythonYear = 1989
 # obliczamy wiek Pythona
 agePython = actYear - pythonYear
 
+# pobieramy dane
+name = input('Jak sie nazywasz? ')
+age = int(input('Ile masz lat? '))
+
 if __name__ == '__main__':
-    print(agePython)
+    # wyświetlamy komunikaty
+    print("Witaj", name)
+    print("Mów mi python, mam", agePython, "lat.")
